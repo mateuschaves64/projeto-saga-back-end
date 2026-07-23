05aa61b2d8c3120569298a080ebe45acdfb88d62
mateuschaves64 <mateus.c.oliveira@ba.estudante.senai.br>
2026-07-22 21:35:21 -0300

    Add files via upload

--- BD TCC/Arquivo completo TCC SAGA.sql
+++ BD TCC/Arquivo completo TCC SAGA.sql
@@ -9,7 +9,9 @@ Revisão da 3FN (Dependência Transitiva): Todos os campos atuais de todas as no
 
 */
 
-
+-- ====================================================================
+-- 1. CRIANDO AS TABELAS + DB: REGISTRO, USUARIO, CURSO USUARIO_CURSO
+-- ====================================================================
 
 CREATE DATABASE tcc_saga_db;
 
@@ -24,4 +26,110 @@ CREATE TABLE usuario(
     preferencia_tema BOOLEAN,
     preferencia_contraste BOOLEAN,
     token_lembrar_me VARCHAR(255)
+);
+
+CREATE TABLE curso(
+	id_curso_pk INT PRIMARY KEY AUTO_INCREMENT,
+    nome_curso VARCHAR(100) NOT NULL,
+    tipo_curso ENUM(
+    'tecnico', 
+    'curso_de_qualificacao_aperfeicoamento_extensao', 
+    'programas'
+    ) NOT NULL
+);
+
+CREATE TABLE usuario_curso(
+	id_usuario_curso_pk INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario_fk INT,
+    id_curso_fk INT,
+    
+    FOREIGN KEY(id_usuario_fk) REFERENCES usuario(id_usuario_pk),
+    FOREIGN KEY(id_curso_fk) REFERENCES curso(id_curso_pk)
+);
+
+CREATE TABLE registro_pedagogico(
+	id_registro_pk INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario_fk INT NOT NULL,
+    id_curso_fk INT,
+    
+    tipo_relatorio ENUM(
+		'atendimento_geral', 
+		'acompanhamento_pedagogico'
+    ) NOT NULL,
+    
+    status_registro ENUM(
+		'pendente', 
+		'andamento', 
+		'finalizado'
+    ) NOT NULL,
+    
+    turno ENUM(
+		'manha', 
+        'tarde', 
+        'noite'
+    ),
+    
+    modalidade ENUM(
+		'presencial', 
+        'ead', 
+        'hibrido'
+    ),
+    
+    data_inicial DATE NOT NULL,
+    data_encerramento DATE NULL,
+    data_conclusao TIMESTAMP,
+    turma VARCHAR(30),
+    
+    tipo_atendimento ENUM(
+		'acolhimento', 
+		'acompanhamento', 
+		'afastamento_temporario', 
+		'advertencia', 
+		'ambientacao_docente', 
+		'aplicacao_de_pesquisa', 
+		'apoio_a_coordenacao', 
+		'atendimento_emergencial', 
+		'boas_vindas', 
+		'calcado_inadequado', 
+		'fardamento_inadequado', 
+		'intervencao_pedagogica', 
+		'miniaula', 
+		'na', 
+		'orientacao_aluno', 
+		'orientacao_reuniao_instrutora', 
+		'outros', 
+		'pais_resp', 
+		'reuniao', 
+		'visita_pedagogica'
+
+    ),
+    
+    tipo_acao ENUM(
+		'acompanhamento_docente_turma', 
+        'docente'
+    ),
+    
+    tipo_contratacao ENUM(
+		'horista', 
+        'mensalista', 
+        'rsa'
+    ),
+    
+    relatorio ENUM(
+		'na', 
+		'arquivado', 
+		'andamento', 
+		'falta_feedback', 
+		'observacoes' 
+    ),
+    
+    pedagogo VARCHAR(120),
+    instrutor_aluno VARCHAR(120),
+    coordenacao VARCHAR(120),
+    docente VARCHAR(120),
+    tratativa_nep TEXT,
+    descricao TEXT,
+    
+    FOREIGN KEY(id_usuario_fk) REFERENCES usuario(id_usuario_pk),
+    FOREIGN KEY(id_curso_fk) REFERENCES curso(id_curso_pk)
 );
